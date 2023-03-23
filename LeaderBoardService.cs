f85bb395a0d0ae260dc4e3e440ac8fd278882eaa
Rick Hodder <rickhodder@msn.com>
2023-03-23 17:57:43 -0400

    Flesh out Player and LeaderBoard

--- LeaderBoardService.cs
+++ LeaderBoardService.cs
@@ -9,8 +9,18 @@ public class LeaderBoardService
 
 public class Player
 {
+    public string PlayerName { get; set; }="";
+    public int Score { get; set; }
 }
 
-public class LeaderBoard
+public class LeaderBoard : List<LeaderBoardEntry>
 {
+}
+
+public class LeaderBoardEntry
+{
+    public int Rank { get; set; }
+    public string Name { get; set; }="";
+
+    public int Score { get; set; }
 }
